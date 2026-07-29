7304d2aa31f863b0eb42f6dea633df36175933ac
Ghost_navigator <gn9172212010@gmail.com>
2026-07-29 11:49:33 +0300

    Настроен GitHub Pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,8 +1,13 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
 
 export default defineConfig({
-  // Отключаем панель разработчика, чтобы она не пыталась искаться по кириллическому пути
+
+  site: "https://gn9172212010-ai.github.io",
+
+  base: "/snt-vozdvizhenskoe4",
+
   devToolbar: {
     enabled: false
   }
+
 });
